60c36cabf73ecb9468503e0b0e012ae275999357
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:04:31 +0000

    Added ?ExtensionInterface

--- EveryWorkflowCmsBundle.php
+++ EveryWorkflowCmsBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\CmsBundle;
 
 use EveryWorkflow\CmsBundle\DependencyInjection\CmsExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowCmsBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new CmsExtension();
     }
